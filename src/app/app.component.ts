faee386430be978a99ebfcbd6d0fe3d274c1da04
Alejandro <Alejandro.gomezg@autonoma.edu.co>
2023-05-04 19:15:30 -0500

    Termination of the project with the dynamic filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { UserService } from './services/user.service';
+import { FormsModule } from '@angular/forms';   
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Opitech';
+
+export class AppComponent implements OnInit {
+  users:any
+  searchQuery: string = '';
+  filteredUsers: any
+  constructor(private userService:UserService){}
+  
+  ngOnInit() {
+    this.userService.getUsers()
+      .subscribe(response => {
+        this.users = response;
+        this.filteredUsers = response;
+        console.log(this.users);
+      });
+  }
+
+  filterUsers() {
+    console.log(this.searchQuery);
+    this.filteredUsers = this.users.filter((user:any) =>
+      user.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+  
+sortByNameAsc() {
+  this.filteredUsers.sort((a: any, b: any) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+sortByNameDesc() {
+  this.filteredUsers.sort((a: any, b: any) => {
+    if (a.name < b.name) {
+      return 1;
+    }
+    if (a.name > b.name) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+
+
+
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private url = 'https://jsonplaceholder.typicode.com/users';
+  constructor(private httpClient: HttpClient) { }
+  getUsers(){
+    return this.httpClient.get(this.url);
+  }
+}
